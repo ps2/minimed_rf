e5f814e9d7e66d2b9a991871fa1671b2b8d9c9bc
Pete Schwamb <pete@schwamb.net>
2014-09-08 09:23:39 -0500

    Device link protocol

--- spec/device_link_spec.rb
+++ spec/device_link_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe MinimedRF::FindDevice do
+describe MinimedRF::DeviceLink do
 
   it "should decode fields" do
-    message = MinimedRF::FindDevice.from_hex("3700069500")
+    message = MinimedRF::DeviceLink.from_hex("3700069500")
     expect(message.sequence).to eq 55
-    expect(message.broadcast_address).to eq "000695"
+    expect(message.device_address).to eq "000695"
   end
 
 end
