0c4ab5cf04ea46f951b8e7e3451d76a80ac25a68
Pete Schwamb <pete@drivetrainagency.com>
2016-03-10 21:26:52 -0600

    better test

--- spec/meter_spec.rb
+++ spec/meter_spec.rb
@@ -16,12 +16,11 @@ describe MinimedRF::Meter do
     expect(message.glucose).to eq 398
   end
 
-  it "should decode fields when bg above 255" do
-    hex_data = "a5c527ad018e77"
+  it "should decode ack bits" do
+    hex_data = "a5c527ad008e61"
     packet = MinimedRF::Packet.from_hex(hex_data)
     message = packet.to_message
     expect(message.is_ack?).to eq false
   end
-
 
 end
