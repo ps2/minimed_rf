5607220ef399742e694252c34bef817e1c7caeff
Pete Schwamb <pete@schwamb.net>
2016-10-24 10:35:10 -0500

    Decode type and duration of ChangeSensorAlarmSilenceConfig

--- lib/minimed_rf/log_entries/change_sensor_alarm_silence_config.rb
+++ lib/minimed_rf/log_entries/change_sensor_alarm_silence_config.rb
@@ -10,13 +10,39 @@ module MinimedRF
         8
       end
 
-      def to_s
-        "ChangeSensorAlarmSilenceConfig #{timestamp_str}"
+      def alert_type_str
+      end
+
+      def alert_type
+        # 0=off, 1=hi, 2=lo, 4=hi_lo, 8=all
+        case d(1)
+        when 0
+          :off
+        when 1
+          :high
+        when 2
+          :low
+        when 4
+          :high_low
+        when 8
+          :all
+        else
+          :unknown
+        end
+      end
+
+      def duration
+        ((d(4) & 0b11100000) << 3) + d(7)
       end
 
       def timestamp
         parse_date(2)
       end
+
+      def to_s
+        "ChangeSensorAlarmSilenceConfig (alert_type=#{alert_type}) (duration=#{duration}m) #{timestamp_str}"
+      end
+
 
     end
   end
